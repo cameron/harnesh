da26b32f925fda8aaf32acfc41837c98c3c18874
Cameron Boehmer <cameron.boehmer@gmail.com>
2026-07-22 15:45:08 -0700

    package harnesh for NixOS

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "PTY-first hybrid terminal and agent harness";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "harnesh";
+            version = "0-unstable-2026-06-24";
+            src = ./.;
+
+            vendorHash = "sha256-ciGayPKX2j48v7nO6TUUCqynF6Q7vHMdnVgz3ZW8bo8=";
+
+            ldflags = [ "-s" "-w" ];
+
+            meta = {
+              description = "PTY-first hybrid terminal and agent harness";
+              homepage = "https://github.com/cameron/harnesh";
+              mainProgram = "harnesh";
+              platforms = nixpkgs.lib.platforms.linux;
+            };
+          };
+        });
+
+      nixosModules.default = { pkgs, ... }: {
+        environment.systemPackages = [
+          self.packages.${pkgs.stdenv.hostPlatform.system}.default
+        ];
+      };
+    };
+}
